b62e64ab4f46eec68b43204d6dfee568b20bacbc
Aivaseda <aivaseda@bk.ru>
2024-11-18 23:21:53 +0500

    add application*, user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	"developmentOnly"("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,42 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String passwordDigest;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
