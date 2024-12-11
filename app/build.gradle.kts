2bbcdc79ac555bced825d7b5631014b03882e9e5
Aivaseda <aivaseda@bk.ru>
2024-12-11 10:18:43 +0500

    completed point 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,15 +10,37 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
     id("com.github.ben-manes.versions") version "0.50.0"
     id("io.freefair.lombok") version "8.6"
+    id ("io.sentry.jvm.gradle") version "4.14.1"
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
+application {
+    mainClass.set("hexlet.code.app.AppApplication")
+}
+
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+tasks.sentryBundleSourcesJava {
+    enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+}
+
+sentry {
+    includeSourceContext = true
+
+    org = "java-vxz"
+    projectName = "java-spring-boot"
+    authToken = System.getenv("SENTRY_AUTH_TOKEN")
 }
 
 configurations {
@@ -31,40 +53,40 @@ repositories {
     mavenCentral()
 }
 
-application {
-    mainClass.set("hexlet.code.app.AppApplication")
-}
+
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
+
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
 
+    implementation("org.projectlombok:lombok")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.projectlombok:lombok")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.postgresql:postgresql")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
     implementation("net.datafaker:datafaker:2.0.2")
     implementation("org.instancio:instancio-junit:3.3.1")
-    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.18.1")
 
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
-    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
+
     runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
